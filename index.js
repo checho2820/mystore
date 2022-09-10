722992735123d389e53684fe80aa46cb4c208314
Sergio Gomez <seluigo@hotmail.com>
2022-09-10 14:06:18 -0500

    Cambiando usuario de Git

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000;
-// Crear un Endpoint en el metodo GET
+// Crear un Endpoint en el metodo GET 
 app.get('', function(req, res){
     res.send("Hola mundo")
 })
